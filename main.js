e1b00c2595443332c2b82e9c05d05a682dd5dc86
swarup1996saha <hoichoi1018@gmail.com>
2023-11-19 21:29:20 +0530

    home page

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 window.addEventListener('scroll', ()=>{
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY >0)
 })
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq =>{
+    faq.addEventListener('click', ()=>{
+        faq.classList.toggle('open')
+
+        const icon = faq.querySelector('.faq_icon i');
+        if(icon.className === 'fa fa-solid fa-plus'){
+            icon.className = "fa fa-solid fa-minus";
+        }else{
+            icon.className = "fa fa-solid fa-plus";
+        }
+    })
+})
